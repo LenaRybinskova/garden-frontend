ff5102632f6468b6c597932f714b3a3a1c24bb5e
«Elena <LenaRybinskova@gmail.com>
2025-07-21 15:33:46 +0300

    add husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2020: true,
+        node: true, // Добавляем node, так как Next.js работает и на сервере
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@next/next/recommended',
+        'prettier',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs', '.next'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    plugins: ['react-refresh', '@typescript-eslint'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'no-undef': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
+};
