103d25aec88764f59493b9e2cedb6640593d5591
EvgeniyaYakunina <Yakoonina@gmail.com>
2025-08-01 17:08:52 +0300

    update tailwind 2.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,13 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'next/core-web-vitals' // добавляем правила Next.js
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs', '.next', 'out'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
@@ -11,6 +16,10 @@ module.exports = {
             { allowConstantExport: true },
         ],
         'no-undef': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn', // изменяем с error на warn
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
     },
 }
 
