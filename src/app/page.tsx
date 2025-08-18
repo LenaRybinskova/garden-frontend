951672817a928ab935a2569a7b9d4db34d9b53b4
«Elena <LenaRybinskova@gmail.com>
2025-08-18 11:51:48 +0300

    commit test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className={'flex min-h-screen w-full bg-blue-400'}>тут будет вся инфа</div>;
+  return (
+    <div className={'flex min-h-screen w-full bg-blue-400'}>
+      тут будет вся инфа<div>тест</div>
+    </div>
+  );
 }
